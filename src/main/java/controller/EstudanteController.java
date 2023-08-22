43d80d763a8c9de150851b6b3be117670194a580
FatimaCalia <142550393+FatimaCalia@users.noreply.github.com>
2023-08-22 08:23:15 +0200

    Update EstudanteController.java

--- src/main/java/controller/EstudanteController.java
+++ src/main/java/controller/EstudanteController.java
@@ -55,12 +55,28 @@ public class EstudanteController {
         estudanteDAO.listar().forEach(estudante -> System.out.println(estudante.toString()));
     }
     //Vou adicionar um comentario so para explicar o commit
-
+// aumentar dois metodos actualizar e remover fazer o commit com o guit   por fim compactar a pasta e submeter
+  
     public void atualizarEstudante(Estudante estudante) {
+        estudante.setnrMatricula("digite o numero de matricula do estudante que deseja actualizar os dados ");
+         estudante.setnome("digite o nome");
+         estudante.setapelido("digite o apelido");
+         estudante.setendereco("digite o endereco);
+           estudante.setendereco("digite o contacto);
+        
         estudanteDAO.atualizar(estudante);
+        
+             System.out.println("Estudante Actualizado com sucesso!");
     }
 
     public void removerEstudante(int nrMatricula) {
-        estudanteDAO.remover(nrMatricula);
+        
+          estudanteDAO.remover(nrMatricula);
+         estudanteDAO.remover( String nome);
+         estudanteDAO.remover(String apelido);
+         estudanteDAO.remover(String endereco);
+           estudanteDAO.remover(String contactio);
+      
+             System.out.println("Estudante removido com sucesso!");
     }
 }
